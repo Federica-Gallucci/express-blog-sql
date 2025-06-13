2f6ce7406dd52aa7a05ca520b2af0d8f7b7b690c
Federica-Gallucci <gallucci.federica.c@gmail.com>
2025-06-13 18:06:29 +0200

    add api show

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -20,6 +20,14 @@ const index = (req, res) => {
 };
 
 const show = (req, res) => {
+  const id = req.params.id;
+  const sql = `SELECT * FROM posts WHERE id =?`;
+  connection.query(sql, [id], (error, results) => {
+    if (err) return res.status(500).json({ error: `Database query failed` });
+    if (results.length === 0)
+      return res.status(404).json({ error: `Post not found` });
+    res.json(result[0]);
+  });
   // const id = parseInt(req.params.id);
   // const post = posts.find((currentPost) => currentPost.id === id);
   // if (!post) {
