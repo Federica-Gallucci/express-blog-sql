889f8dfdbc923c30446bd111552b6fdb0c995298
Federica-Gallucci <gallucci.federica.c@gmail.com>
2025-06-13 16:37:37 +0200

    api index reurns posts's list

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -2,9 +2,20 @@
 const connection = require("../data/db");
 
 const index = (req, res) => {
+  const sql = `SELECT * FROM posts`;
+  connection.query(sql, (err, results) => {
+    // if (err) throw err;
+    // console.log(results);
+    if (err)
+      return res.status(500).json({
+        error: `Database query failed`,
+      });
+    res.json(results);
+  });
   // res.json({
   //   description: "Lettura della lista di post",
-  //   data: posts,
+  //   data: results,
+  //   status: 200,
   // });
 };
 
